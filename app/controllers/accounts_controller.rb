d3913522416ba0e0492c6c1a450965c5f11a5521
Kris Hill <krish@ricksteves.com>
2025-03-19 10:42:26 -0700

    creating basic accounts actions

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,18 @@
 class AccountsController < ApplicationController
   def home
   end
+
+  def new
+    @account = Account.new
+  end
+
+  def create
+    @account = Account.new(account_params)
+
+    if @account.save
+      redirect_to @account
+    else
+      render 'new'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "accounts#home"
+
+  resources :accounts
 end
